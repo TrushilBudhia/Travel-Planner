33473321b4655e0aab187ab60544d542a7551f60
Trushil <trushil.budhia@gmail.com>
2021-07-19 23:02:39 +0800

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,1 +1,11 @@
 
+const router = require('express').Router();
+const locationRoutes = require('./location-routes');
+const travellersRoutes = require('./travellers-routes');
+const tripsRoutes = require('./trips-routes');
+
+router.use('/locations', locationRoutes);
+router.use('/travellers', travellersRoutes);
+router.use('/trips', tripsRoutes);
+
+module.exports = router;
