59ec93ed402695ff8d89e872cb99d70584956991
Trushil <trushil.budhia@gmail.com>
2021-09-20 09:55:27 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,1 +1,21 @@
+const Traveller = require('./travllers);
+const Location = require('./locations);
+const Trip = require('./trips);
+                          
+Traveller.belongsToMany(Location, {
+  through: {
+    model: Trip,
+    unique: false
+},
+  as: 'planned_trips'
+});
 
+Location.belongsToMany(Traveller, {
+  through: {
+    model: Trip,
+    unique: false
+  },
+  as: 'location_travellers'
+});
+
+module.exports = { Traveller, Location, Trip };
