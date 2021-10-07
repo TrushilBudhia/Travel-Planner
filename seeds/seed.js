5b4cd88241b4780cb70a9c397cc1b43f6395677c
Trushil Budhia <trushil.budhia@gmail.com>
2021-10-07 18:50:23 +0800

    Updated seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -11,6 +11,26 @@ const seedDatabase = async () => {
 
     const locations = await Location.bulkCreate(locationSeedData);
 
+    for (let i = 0; i < 10; i++) {
+        // Get a random traveller's id
+        const travellersIndex = Math.floor(Math.random() * travellers.length);
+        const { id: randomTravellerId } = travellers[travellersIndex];
+
+        // Get a random location's id
+        const locationsIndex = Math.floor(Math.random() * locations.length);
+        const { id: randomLocationId } = locations[locationsIndex];
+
+        // Create a new trip with random values
+        await Trip.create({
+            trip_budget: (Math.random() * 10000 + 1000).toFixed(2),
+            traveller_amount: Math.floor(Math.random() * 10) + 1,
+            traveller_id: randomTravellerId,
+            location_id: randomLocationId
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
+
     process.exit(0);
 };
 
